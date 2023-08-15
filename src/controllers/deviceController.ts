399c1d475c504d310e052d0875ac43bade57240e
khang <khanghero123@gmail.com>
2023-08-15 21:25:25 +0700

    #4 - Link new device and get device by id

--- src/controllers/deviceController.ts
+++ src/controllers/deviceController.ts
@@ -1,14 +1,39 @@
 import { Request, Response } from 'express'
 
+import LinkDeviceRequest from '@/models/requests/linkDeviceRequest'
 import deviceService from '@/services/deviceService'
+
+import isLinkRequestValid from '@/utils/validateLinkRequest'
 
 const getAllDevices = async (_: Request, res: Response) => {
   try {
     const deviceList = await deviceService.getDeviceList()
     res.json(deviceList)
   } catch (error) {
-    res.status(500).send({ error: error })
+    if (error instanceof Error) res.status(500).send({ error: error.message })
   }
 }
 
-export default { getAllDevices }
+const getSingleDevice = async (req: Request, res: Response) => {
+  const deviceId: string = req.params.id
+
+  try {
+    const device = await deviceService.getSingleDevice(deviceId)
+    res.json(device)
+  } catch (error) {
+    if (error instanceof Error) res.status(400).send({ message: error.message })
+  }
+}
+
+const linkNewDevice = async (req: Request, res: Response) => {
+  try {
+    const newDevice = <LinkDeviceRequest>req.body
+    if (!isLinkRequestValid(newDevice)) throw Error('Missing field(s)')
+    await deviceService.createNewDevice(newDevice)
+    res.json({ message: 'Successfully linked new device' })
+  } catch (error) {
+    if (error instanceof Error) res.status(400).send({ message: error.message })
+  }
+}
+
+export default { getAllDevices, getSingleDevice, linkNewDevice }
--- src/services/deviceService.ts
+++ src/services/deviceService.ts
@@ -1,3 +1,4 @@
+import LinkDeviceRequest from '@/models/requests/linkDeviceRequest'
 import DeviceModel, { IDevice } from '@/models/deviceModel'
 
 type DeviceList = IDevice[]
@@ -14,6 +15,32 @@ const getDeviceList = async (): Promise<DeviceList> => {
   return deviceList
 }
 
+const getSingleDevice = async (deviceId: string): Promise<IDevice | null> => {
+  type Device = IDevice | null
+  let device: Device = null
+  try {
+    device = await DeviceModel.findById(deviceId)
+    if (device != null) {
+      return device
+    }
+  } catch (error) {
+    throw new Error(`Cannot find device with the id ${deviceId}`)
+  }
+
+  return device
+}
+
+const createNewDevice = async (newDevice: LinkDeviceRequest) => {
+  try {
+    const newDeviceModel = new DeviceModel(newDevice)
+    await newDeviceModel.save()
+  } catch (error) {
+    throw new Error(`Cannot save new device`)
+  }
+}
+
 export default {
-  getDeviceList
+  getDeviceList,
+  getSingleDevice,
+  createNewDevice
 }
